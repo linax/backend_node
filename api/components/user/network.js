65a7d94a6534ba57e94aa6ec9c3bda46142f2a58
linax <c.lagosv@gmail.com>
2024-09-01 20:30:39 -0400

    add dummy db and controller to get data

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const Controller =  require('./controller');
 
 const router =  express.Router();
 
 router.get('/', (req, res) => {
-    response.success(req, res, 'Todo correcto', 200)
+    const list = Controller.list();
+    response.success(req, res, list, 200)
 });
 
 module.exports = router;
